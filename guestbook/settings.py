2d107383372e31ff708a9cda2fb23ed2764e017b
Tiago Ilieve <tiago.myhro@gmail.com>
2015-04-08 22:07:46 -0300

    Add database configuration

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,11 +75,10 @@ WSGI_APPLICATION = 'guestbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+SQLITE_DB = os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///' + SQLITE_DB)
 }
 
 
