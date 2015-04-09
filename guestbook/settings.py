d86016c1083f67579012daf213d2a31275e26436
Tiago Ilieve <tiago.myhro@gmail.com>
2015-04-09 00:00:22 -0300

    ALLOWED_HOSTS and APP_DIRS configuration

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
+
+APP_DIRS = True
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
