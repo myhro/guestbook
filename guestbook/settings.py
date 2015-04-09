fc41841afbd7123e8cfda0de5b00d772c20ba3c3
Tiago Ilieve <tiago.myhro@gmail.com>
2015-04-08 23:59:47 -0300

    STATIC and MEDIA files configuration

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -99,4 +99,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- guestbook/urls.py
+++ guestbook/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -8,3 +10,6 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
