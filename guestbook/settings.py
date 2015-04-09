391de88cfff60483be5fd9ac017f2129a969d56f
Tiago Ilieve <tiago.myhro@gmail.com>
2015-04-08 22:20:18 -0300

    Add dotenv support

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#d#96kmphed!6&&2+pa*!6vc^xd#lo1+v)=#k5n!o)*dsbd=z('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
